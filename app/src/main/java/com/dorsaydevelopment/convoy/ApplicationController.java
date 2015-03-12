e47842356858b1327c833a08f9ed4739249a582b
Brycen <dorsay@live.ca>
2015-03-11 20:38:53 -0700

    Push notifications set up

--- app/src/main/java/com/dorsaydevelopment/convoy/ApplicationController.java
+++ app/src/main/java/com/dorsaydevelopment/convoy/ApplicationController.java
@@ -1,21 +1,19 @@
 package com.dorsaydevelopment.convoy;
 
-import android.app.Application;
-
 import com.parse.Parse;
 
 /**
  * Created by brycen on 15-03-11.
  */
-public class ApplicationController extends Application {
+public class ApplicationController extends android.app.Application {
 
-    private static Application singletonInstance;
+    private static android.app.Application singletonInstance;
 
     public void onCreate() {
         Parse.initialize(this, "kpAPxWNrBwAomp48pDgQNFFXrpzsubCRpblNlLFd", "9On6nF8dkMmsMNnAgQvqwJDYAyF55hDyX4lXbnmv");
     }
 
-    public static synchronized Application getInstance() {
+    public static synchronized android.app.Application getInstance() {
         return singletonInstance;
     }
 
