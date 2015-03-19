1fb60aa9d182655072b9d2afc769710ad8da1005
Brycen Dorsay <dorsay@live.ca>
2015-03-19 14:26:59 -0700

    Create group dialog

--- app/src/main/java/com/dorsaydevelopment/convoy/MainActivity.java
+++ app/src/main/java/com/dorsaydevelopment/convoy/MainActivity.java
@@ -1,16 +1,20 @@
 package com.dorsaydevelopment.convoy;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -105,13 +109,6 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
     };
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
         if(ParseUser.getCurrentUser() == null) {
@@ -123,6 +120,15 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -132,6 +138,29 @@ public class MainActivity extends ActionBarActivity implements SwipeRefreshLayou
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        } else if(id == R.id.action_new_group) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("New Group");
+            final EditText input = new EditText(this);
+            input.setInputType(InputType.TYPE_CLASS_TEXT);
+            builder.setView(input);
+            builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(), GroupActivity.class);
+                    intent.putExtra("group_name", input.getText().toString());
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.show();
             return true;
         }
 
