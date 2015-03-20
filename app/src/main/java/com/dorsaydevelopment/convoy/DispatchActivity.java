e3bafae5cab0ab5394d2936e8d3fba1be1b9f5c5
Brycen Dorsay <dorsay@live.ca>
2015-03-19 21:34:41 -0700

    Removed unneeded dispatcher activity visuals

--- app/src/main/java/com/dorsaydevelopment/convoy/DispatchActivity.java
+++ app/src/main/java/com/dorsaydevelopment/convoy/DispatchActivity.java
@@ -3,8 +3,6 @@ package com.dorsaydevelopment.convoy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.facebook.AppEventsLogger;
 import com.parse.ParseUser;
@@ -38,27 +36,5 @@ public class DispatchActivity extends ActionBarActivity {
         super.onPause();
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_dispatch, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
