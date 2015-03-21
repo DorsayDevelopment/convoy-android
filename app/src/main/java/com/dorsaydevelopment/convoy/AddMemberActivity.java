3878d0db7e98be15c446483fc0d6cc6119585a3e
Brycen Dorsay <dorsay@live.ca>
2015-03-21 02:16:16 -0700

    Searching by exact match username

--- app/src/main/java/com/dorsaydevelopment/convoy/AddMemberActivity.java
+++ app/src/main/java/com/dorsaydevelopment/convoy/AddMemberActivity.java
@@ -10,14 +10,27 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.ParseUser;
 
 
 public class AddMemberActivity extends ActionBarActivity {
+
+    private ParseQueryAdapter adapter;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_member);
+
+        listView = (ListView) findViewById(R.id.search_users_listview);
     }
 
 
@@ -37,17 +50,49 @@ public class AddMemberActivity extends ActionBarActivity {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 Log.i("Search", "String submitted > " + s);
+                populateUsersList(s);
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
                 Log.i("Search", "String updated > " + s);
+                populateUsersList(s);
                 return false;
             }
         });
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void populateUsersList(final String queryString) {
+
+        ParseQueryAdapter.QueryFactory<ParseUser> factory = new ParseQueryAdapter.QueryFactory<ParseUser>() {
+            @Override
+            public ParseQuery<ParseUser> create() {
+                ParseQuery<ParseUser> queryUsername = ParseUser.getQuery();
+                queryUsername.whereEqualTo("username", queryString);
+                return queryUsername;
+            }
+        };
+
+        adapter = new ParseQueryAdapter<ParseUser>(this, factory) {
+            @Override
+            public View getItemView(ParseUser user, View view, ViewGroup parent) {
+                if(view == null) {
+                    view = View.inflate(getContext(), R.layout.user_list_item, null);
+                }
+                TextView name = (TextView) view.findViewById(R.id.text_view_user_name);
+                if(user.getUsername().length() == 25)
+                    name.setText(user.get("firstName") + " " + user.get("lastName"));
+                else
+                    name.setText(user.getUsername());
+
+                return view;
+            }
+        };
+
+        listView.setAdapter(adapter);
     }
 
     @Override
