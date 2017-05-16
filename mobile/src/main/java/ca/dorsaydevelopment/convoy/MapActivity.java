d844474693dfb694d4672c06e78b557ec5307d16
Brycen <dorsay@live.ca>
2017-05-15 21:57:53 -0700

    Location services on map activity

--- mobile/src/main/java/ca/dorsaydevelopment/convoy/MapActivity.java
+++ mobile/src/main/java/ca/dorsaydevelopment/convoy/MapActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -31,14 +32,17 @@ public class MapActivity extends FragmentActivity implements
         LocationListener {
 
 
-    private GoogleMap mMap;
+    private GoogleMap map;
     private static final boolean requestingUpdates = false;
     private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+    private Location initialLocation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("debug", "onCreate()");
         setContentView(R.layout.activity_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -47,21 +51,36 @@ public class MapActivity extends FragmentActivity implements
 
         if (googleApiClient == null) {
             googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
                     .addApi(LocationServices.API)
                     .build();
         }
 
     }
 
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d("debug", "onPause()");
         stopLocationUpdates();
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        Log.d("debug", "onResume()");
         if (googleApiClient.isConnected() && !requestingUpdates) {
             startLocationUpdates();
         }
@@ -79,49 +98,71 @@ public class MapActivity extends FragmentActivity implements
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        Log.d("debug", "onMapReady()");
+        map = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        Log.d("debug", "onConnected()");
         startLocationUpdates();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
+        Log.d("debug", "onConnectionSuspended()");
 
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        Log.d("debug", "onConnectionFailed()");
     }
 
     @Override
     public void onLocationChanged(Location location) {
         Log.d("debug", "Location updated: " + location);
+        lastLocation = location;
+        if(initialLocation == null) {
+            initialLocation = location;
+            LatLng latLng = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+//            map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(latLng, 15, 0, 0)));
+        }
+
     }
 
     protected void startLocationUpdates() {
+        Log.d("debug", "startLocationUpdates()");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissions, 1);
             return;
         }
 
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
-        locationRequest.setFastestInterval(1 * 60000);
-        locationRequest.setInterval(5 * 60000);
+        map.setMyLocationEnabled(true);
+        LocationRequest locationRequest = createLocationRequest();
         LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
     }
 
     protected void stopLocationUpdates() {
+        Log.d("debug", "stopLocationUpdates()");
         if(googleApiClient != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
 
         }
+    }
+
+    protected LocationRequest createLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+        locationRequest.setFastestInterval(1 * 60000);
+        locationRequest.setInterval(5 * 60000);
+        return locationRequest;
     }
 }
